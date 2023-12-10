cb361bb2a60c27de76636e19cc1bcfd303a5e8f6
stereoIII6 <stereodocbush@gmail.com>
2023-12-10 23:47:42 +0100

    push info

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -1,17 +1,18 @@
 export const iii6_stage = `
 <div id='oracles' class='boxpost1'>
     <h1 id='' class='title'>Real World Data Oracles</h1>
-    <div id='' class='txt'></div>
+    <div id='' class='txt'>
+Real-world data oracles in Web3 bridge blockchain with external info for decentralized apps. Chainlink, a key player, ensures trust via node consensus. Vital for DApps' reliance on real-world events. #Web3 #DataOracles</div>
     <img src='' class='img' />
 </div>
 <div id='external' class='boxpost2'>
     <h1 id='' class='title'>Trigger external Contracts</h1>
-    <div id='' class='txt'></div>
+    <div id='' class='txt'>DIAS Assets interact with external contracts, enhancing capabilities and data exchange. Smart contracts communicate for a decentralized network, fostering collaboration in the blockchain ecosystem. #DApps #SmartContracts #BlockchainInteroperability</div>
     <img src='' class='img' />
 </div>
 <div id='dynft' class='barpost'>
     <h1 id='' class='title'>Design, Build & Launch dynamic NFTs</h1>
-    <div id='' class='txt'></div>
+    <div id='' class='txt'>Designing, building, and launching Dynamic NFTs involves creative ideation, smart contract development, and strategic deployment. This process merges art with functionality, offering unique, programmable digital assets that evolve over time. #DynamicNFTs #BlockchainArt</div>
     <img src='' class='img' />
 </div>
 <div id='diasinf' class='barpost'>
@@ -33,17 +34,18 @@ export const design_stage = `
 export const develop_stage = `
 <div id="coinbuild" class="barpost">
   <h1 id="" class="title">Create a custom ERC20 Token Contract</h1>
-  <div id='' class='txt'></div>
+  <div id='' class='txt'>Crafting custom ERC-20 tokens with intricate tokenomics requires precision in smart contract design. Define supply, distribution rules, and governance mechanisms, ensuring a comprehensive and secure foundation for a token ecosystem tailored to specific needs. #Tokenomics #ERC20Tokens</div>
   <img src='' class="img" />
 </div>
 <div id="nftbuild" class="barpost">
   <h1 id="" class="title">Create a custom dynamic NFT Contract</h1>
-  <div id='' class='txt'></div>
+  <div id='' class='txt'>
+Developing custom dynamic NFT contracts demands meticulous coding. Integrate features for evolving attributes, programmable behaviors, and interactive elements. Smart contract precision ensures a unique, engaging, and adaptive NFT experience. #DynamicNFTs #SmartContracts</div>
   <img src='' class="img" />
 </div>
 <div id="sftbuild" class="barpost">
   <h1 id="" class="title">Create a custom dynamic SFT Contract</h1>
-  <div id='' class='txt'></div>
+  <div id='' class='txt'>Crafting bespoke dynamic ERC-1155 SFT contracts demands intricate smart contract development. Integrate adaptive features for semi-fungible tokens, ensuring unique and programmable behaviors. Precision coding shapes a versatile and engaging NFT ecosystem. #ERC1155 #SmartContracts</div>
   <img src='' class="img" />
 </div>`;
 export const launch_stage = `
