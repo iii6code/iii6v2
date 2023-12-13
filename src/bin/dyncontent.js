2b94890ac0fd51f2870bf2424786376263d5f089
stereoIII6 <stereodocbush@gmail.com>
2023-12-13 17:34:33 +0100

    home

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -1,12 +1,11 @@
 export const iii6_stage = `
 <div id='oracles' class='boxpost1'>
-    <h1 id='' class='title'>Real World Data Oracles</h1>
+    <h1 id='' class='title'>The Interactive dApp Framework</h1>
     <div id='' class='txtfull'>
 Real-world data oracles in Web3 bridge blockchain with external info for decentralized apps. Chainlink, a key player, ensures trust via node consensus. Vital for DApps' reliance on real-world events. #Web3 #DataOracles</div>
-    <img src='./images/delphi.jpeg' class="imgfull" />
 </div>
 <div id='external' class='boxpost2'>
-    <h1 id='' class='title'>Trigger external Contracts</h1>
+    <h1 id='' class='title'>Tokenise your Products & Services</h1>
     <div id='' class='txtfull'>DIAS Assets interact with external contracts, enhancing capabilities and data exchange. Smart contracts communicate for a decentralized network, fostering collaboration in the blockchain ecosystem. #DApps #SmartContracts #BlockchainInteroperability</div>
     <img src='' class='imgfull' />
 </div>
@@ -35,18 +34,53 @@ export const develop_stage = `
 <div id="coinbuild" class="barpost">
   <h1 id="" class="title">Create a custom ERC20 Token Contract</h1>
   <div id='' class='txt'>Crafting custom ERC-20 tokens with intricate tokenomics requires precision in smart contract design. Define supply, distribution rules, and governance mechanisms, ensuring a comprehensive and secure foundation for a token ecosystem tailored to specific needs. #Tokenomics #ERC20Tokens</div>
-  <img src='' class="img" />
+  <div id=form class=list >  
+  <i>Contract Name</i>
+  <input type=text id=cname placeholder="Contract Name" class=devinp />
+  <i>Ticker Symbol</i>
+  <input type=text id=csym placeholder="$ymbol" class=devinp />
+  <i>Max Token Amount</i>
+  <input type=number id=camnt min=1 max=1000000000 class=devinp />
+  <i>Price</i>
+  <input type=number id=cprice min=1 max=1000000000 class=devinp />
+  <i></i>
+  <button>Create Contract</button>
+</div>
 </div>
 <div id="nftbuild" class="barpost">
   <h1 id="" class="title">Create a custom dynamic NFT Contract</h1>
   <div id='' class='txt'>
 Developing custom dynamic NFT contracts demands meticulous coding. Integrate features for evolving attributes, programmable behaviors, and interactive elements. Smart contract precision ensures a unique, engaging, and adaptive NFT experience. #DynamicNFTs #SmartContracts</div>
-  <img src='' class="img" />
+<div id=form class=list >  
+  <i>Contract Name</i>
+  <input type=text id=cname placeholder="Contract Name" class=devinp />
+  <i>Ticker Symbol</i>
+  <input type=text id=csym placeholder="$ymbol" class=devinp />
+  <i>Max Token Amount</i>
+  <input type=number id=camnt min=1 max=1000000000 class=devinp />
+  <i>Wallet Mint Limit</i>
+  <input type=number id=clim min=1 max=1000000000 class=devinp />
+  <i>Price</i>
+  <input type=number id=cprice min=1 max=1000000000 class=devinp />
+  <i></i>
+  <button>Create Contract</button>
+</div>
 </div>
 <div id="sftbuild" class="barpost">
   <h1 id="" class="title">Create a custom dynamic SFT Contract</h1>
   <div id='' class='txt'>Crafting bespoke dynamic ERC-1155 SFT contracts demands intricate smart contract development. Integrate adaptive features for semi-fungible tokens, ensuring unique and programmable behaviors. Precision coding shapes a versatile and engaging NFT ecosystem. #ERC1155 #SmartContracts</div>
-  <img src='' class="img" />
+  <div id=form class=list >  
+  <i>Ticker Symbol</i>
+  <input type=text id=csym placeholder="$ymbol" class=devinp />
+  <i>Max Items</i>
+  <input type=number id=camnt min=1 max=1000000000 class=devinp />
+  <i>Max Copys</i>
+  <input type=number id=clim min=1 max=1000000000 class=devinp />
+  <i>Price</i>
+  <input type=number id=cprice min=1 max=1000000000 class=devinp />
+  <i></i>
+  <button>Create Contract</button>
+</div>
 </div>`;
 export const launch_stage = `
 <div id="oracles" class="boxpost1">
