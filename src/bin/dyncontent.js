0da2963126a8015e3759309bba0a235a8b5c7be7
stereoIII6 <stereodocbush@gmail.com>
2023-12-24 20:03:30 +0100

    sunny munny

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -18,12 +18,12 @@ export const iii6_stage = `
 </div>`;
 export const design_stage = `
 <div id='desdyn' class='boxpost1'>
-    <h1 id='' class='title'>Dynamic NFTs Editor</h1>
+    <h1 id='' class='title'>Create dynamic NFTs in 2D or 3D</h1>
     <div id='' class='txt'></div>
     <img src='' class='img' />
 </div>
 <div id='desdapps' class='boxpost2'>
-    <h1 id='' class='title'>Interactive dApp SDK</h1>
+    <h1 id='' class='title'>Create an dynamic Music Album</h1>
     <div id='' class='txt'></div>
     <img src='' class='img' />
 </div>`;
@@ -95,7 +95,8 @@ AI-powered smart contract and DApp audits offer enhanced security. Automated too
   <div id='' class='txtfull'>Multichain Tokenomics involves strategic design across diverse blockchains. Tailor supply, distribution, and governance for efficiency. Development requires precise smart contract coding to ensure seamless interaction and interoperability, fostering a resilient token ecosystem. #MultichainTokenomics #SmartContractDevelopment</div>
   <img src='' class="img" />
 </div>`;
-export const info_stage = `<div id="infotab" class="barpost">
+export const info_stage = `
+<div id="infotab" class="barpost">
   <h1 id="" class="title">iii6 & Friends</h1>
   <div id='' class='txt'></div>
   <img src='' class="imgfull" />
@@ -105,3 +106,76 @@ export const info_stage = `<div id="infotab" class="barpost">
   <div id='' class='txt'></div>
   <img src='./images/logo.png' class="img" />
 </div>`;
+export const _2d_stage = ``;
+export const _3d_stage = ``;
+export const album_stage = `
+<div id="albumset" class="albumset">
+  <input type="" class="full" id="" placeholder="Band Name" />
+  <input type="" class="full" id="" placeholder="Album Title" />
+  <input type="" class="full" id="" placeholder="Production" />
+  <input type="" class="full" id="" placeholder="Credits" />
+  <input type="date" class="half1" id="" />
+  <input type="time" class="half2" id="" placeholder="Album Title" />
+  <i id="label">Release Date</i>
+  <textarea id="" class="full" placeholder="Legal"></textarea>
+</div>
+<div id="" class="trackset">
+  <input type="text" id="" placeholder="Track Name" class="full"/>
+  <input type="text" id="" placeholder="Artists" class="full"/>
+  <input type="text" id="" placeholder="Credits" class="full"/>
+  <textarea id="" class="full" placeholder="Legal"></textarea>
+  <input type="file" id="" class="fUp"/>
+  <div class="upBtn" id="">^</div>
+  <div class="move" id="">.<br/>.<br/>.<br/>.</div>
+  <div class="hidTrack">
+    <input type=text id="trackname0" class="tname" placeholder="Audio File" disabled />
+    <input type="checkbox" id="isHid_0" class="isHid"/>
+    
+    <i id="label">Hidden Track </i>
+    <select class="hidCond" id="hidC1">
+      <option id="manual" value="manual"> Manual Release </option>
+      <option id="chrono" value="chrono"> Timer Release </option>
+      <option id="stats" value="stats"> Album Stats </option>
+      <option id="social" value="social"> Social Growth </option>
+      <option id="custom" value="custom"> Custom Logic </option>
+    </select>
+    <div class="chrono_ext">
+      <input type="date" id="" />
+      <input type="time" id="" />
+    </div>
+    <div class="stats_ext">
+      <input type="range" min=100000 max=1000000 step=10000 id="" />
+      <input type="time" id="" />
+    </div>
+    <div class="social_ext">
+      <input type="text" id="" placeholder="Twitter" />
+      <input type="text" id="" placeholder="Rumble" />
+      <input type="text" id="" placeholder="Twitch" />
+      <input type="text" id="" placeholder="Discord" />
+    </div>
+     <div class="custom_ext">
+      <input type="text" id="" placeholder="Twitter" />
+      <input type="text" id="" placeholder="Rumble" />
+      <input type="text" id="" placeholder="Twitch" />
+      <input type="text" id="" placeholder="Discord" />
+    </div>
+  </div>
+</div>
+<div class="add" id="">+</div> <i id="label">Next Track</i>
+<div class="coverset" id="">
+  <div id="coverroll" class="roll">
+    <div id="roller" class="roller">
+      <div class="coverImg" id="">
+        <img src="" class="cImg" id="" />
+        <input type="file" class="cUp" id="" />
+        <div class="addArt" id="">+</div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="upArt" id=""></div>
+</div>`;
+export const tokenize_stage = ``;
+export const test_stage = ``;
+export const audit_stage = ``;
+export const multi_stage = ``;
