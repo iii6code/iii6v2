5a07168835ec8a92d0060e5c91f7dd29d2437dab
stereoIII6 <stereodocbush@gmail.com>
2023-12-27 03:35:30 +0100

    shop halfly

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -1,6 +1,39 @@
 export const lp_stage = ``;
 export const explore_stage = ``;
-export const shop_stage = ``;
+export const shop_stage = `
+<div id="shop" class="stage">
+<div id="sidebar" class="fullheight"></div>
+<div id="shopnav" class="menu">
+    <div id="filter"></div>
+    <div id="search">
+        <input type="text" id="seek" class="sbar" placeholder="search" />
+        <input type="button" id="seekbtn" class="sbtn" placeholder="search" />
+    </div>
+    <div id="sort">
+        <select id="sort" class="sel">
+            <option id="priceasc" value="priceasc" >Price Asc.</option>
+            <option id="pricedesc" value="pricedesc" >Price Desc.</option>
+            <option id="newasc" value="newasc">Newest</option>
+            <option id="newdesc" value="newdesc">Oldest</option>
+        </select>
+    </div>
+</div>
+<div id="itemlist" class="items">
+    <div id="item1" class="item">
+        <img src="./images/dias/items/0.png" id="img1" class="itempic" />
+        <div id="title" class="title">ITEM 1</div>
+        <div id="price1" class="price">55Matic</div>
+
+    </div>
+    <div id="item2" class="item"></div>
+</div>
+<div id="pages" class="menu">
+    <div id="prev"></div>
+    <div id="current"></div>
+    <div id="next"></div>
+</div>
+</div>
+`;
 export const create_stage = ``;
 export const essence_stage = ``;
 export const _2d_stage = ``;
