e4cac036a4cd45a8739a3c0fe522b55ef5d79bd0
stereoIII6 <stereodocbush@gmail.com>
2024-01-24 15:16:13 +0100

    Update dyncontent.js

--- src/bin/dyncontent.js
+++ src/bin/dyncontent.js
@@ -1,4 +1,6 @@
-export const lp_stage = `<div id="lp" class="stage"></div>`;
+export const lp_stage = `<div id="lp" class="stage">
+
+</div>`;
 export const explore_stage = ``;
 export const shop_stage = `<div id="shop" class="stage">
 <div id="sidebar" class="fullheight"></div>
@@ -83,7 +85,68 @@ export const shop_stage = `<div id="shop" class="stage">
 </div>
 </div>`;
 export const create_stage = ``;
-export const essence_stage = ``;
+export const essence_stage = `<div id="essence" class="stage">
+<div id="sidebar" class="fullheight"></div>
+<div id="shopnav" class="menu">
+    <div id="filter">
+        <input type="checkbox" id="new" class="chb" /><i>New</i>
+        <input type="checkbox" id="star" class="chb" /><i>Star</i>
+        <input type="checkbox" id="dlx" class="chb" /><i>Dlx</i>
+    </div>
+    <div id="search">
+        <input type="text" id="seek" class="sbar" placeholder="search" />
+        <input type="button" id="seekbtn" class="sbtn" />
+    </div>
+    <div id="sort">
+        <select id="sort" class="sel">
+            <option id="priceasc" value="priceasc" >Price Asc.</option>
+            <option id="pricedesc" value="pricedesc" >Price Desc.</option>
+            <option id="newasc" value="newasc">Newest</option>
+            <option id="newdesc" value="newdesc">Oldest</option>
+        </select>
+        <input type="button" id="layout" class="mode"/>
+    </div>
+</div>
+<div id="itemlist" class="items">
+    <div id="item1" class="item">
+        <img src="./images/dias/items/0.png" id="img1" class="itempic" />
+        <div id="title1" class="title">ITEM 1<hr>this is the description</div>
+        <div id="price1" class="price">55Matic</div>
+    </div>
+    <div id="item2" class="item">
+        <img src="./images/dias/items/0.png" id="img2" class="itempic" />
+        <div id="title2" class="title">ITEM 2<hr>this is the description</div>
+        <div id="price2" class="price">55Matic</div>
+    </div>
+    <div id="item3" class="item">
+        <img src="./images/dias/items/0.png" id="img3" class="itempic" />
+        <div id="title3" class="title">ITEM 3<hr>this is the description</div>
+        <div id="price3" class="price">55Matic</div>
+    </div>
+    <div id="item4" class="item">
+        <img src="./images/dias/items/0.png" id="img4" class="itempic" />
+        <div id="title4" class="title">ITEM 4<hr>this is the description</div>
+        <div id="price4" class="price">55Matic</div>
+    </div>
+</div>
+<div id="pages" class="menu">
+    <div id="prev">
+    <input type="button" id="ftab" value="1" class="sbtn" />
+    <input type="button" id="first" value="<<" class="sbtn" />
+    <input type="button" id="ptab" value="10" class="sbtn" />
+    <input type="button" id="prev" value="<" class="sbtn" />
+    </div>
+    <div id="current">
+        <input type="button" id="next" value="Page 11" class="sbtn" />
+    </div>
+    <div id="next">
+    <input type="button" id="next" value=">" class="sbtn" />
+    <input type="button" id="ntab" value="12" class="sbtn" />
+    <input type="button" id="last" value=">>" class="sbtn" />
+    <input type="button" id="ltab" value="99" class="sbtn" />
+    </div>
+</div>
+</div>`;
 export const _2d_stage = ``;
 export const _3d_stage = ``;
 export const album_stage = ``;
