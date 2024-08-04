e2fe5c4dafe5f792c2d317a18fb526d1de76f429
stereoIII6 <stereodocbush@gmail.com>
2024-08-04 13:04:01 +0200

    css update

--- src/bin/administration.js
+++
@@ -1,7 +0,0 @@
-export const createProject = "";
-export const editProject = "";
-export const createTeam = "";
-export const editTeam = "";
-export const editUser = "";
-export const createCoin = "";
-export const createAsset = "";
--- src/bin/contracts.js
+++
@@ -1,5 +0,0 @@
-export const s0x = require("../../dist/contracts/s0xFactory.json");
-export const Friends = require("../../dist/contracts/s0xFriends.json");
-export const Groups = require("../../dist/contracts/s0xGroups.json");
-export const AssetFactory = require("../../dist/contracts/iii6AssetFactory.json");
-export const CoinFactory = require("../../dist/contracts/iii6CoinFactory.json");
