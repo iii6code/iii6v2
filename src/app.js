863e30eed176905fe68212bbdf2718804633ecd3
stereoIII6 <stereodocbush@gmail.com>
2024-08-04 12:37:14 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -41,3 +41,9 @@ const clickUD = (e) => {
   window.open("https://unstoppabledomains.com");
 };
 
+
+$("#mobile_code").intlTelInput({
+	initialCountry: "de",
+	separateDialCode: true,
+	// utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.4/js/utils.js"
+});
